9e49f2dbc746e8c8ba89c052ac039ebb326f8cff
Leonardo Vinicius <100011077+Pokernol@users.noreply.github.com>
2024-04-01 22:55:33 -0300

    First Commit do projeto de Segundo programa orientado a objetos para classificar o nível de censura de um depoimento

---
+++ src/main/java/br/com/fatec/Main.java
@@ -0,0 +1,10 @@
+package main.java.br.com.fatec;
+
+import main.java.br.com.fatec.depoimento.Depoimento;
+
+public class Main {
+    public static void main(String[] args) {
+        Depoimento depoimento = new Depoimento();
+        depoimento.executarPrograma();
+    }
+}
---
+++ src/main/java/br/com/fatec/depoimento/Depoimento.java
@@ -0,0 +1,22 @@
+package main.java.br.com.fatec.depoimento;
+
+public class Depoimento {
+    public void executarPrograma() {
+        //Mensagens de interação com o usuario
+        //Solicitação de Nome
+        //Solicitação de Depoimento
+        //Criar variavel para receber resposta(string)
+        //Chama função de Classificação de Depoimento
+        //Guarda resposta da função
+        //Exibir nivel de cencura
+    }
+    public String ClassificacaoDeDepoimento(String depoimento){
+        //receber o depoimento
+        //Procurar palavras de odio
+        //Contar quantas tem
+        //Classificar nivel para censura
+        //devolver nivel de censura (string)
+        return depoimento;
+    }
+}
+
